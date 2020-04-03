8a02b5bfc0322f73f86b1d09d04faeb17f0f1ebe
lost-dream <13623673118@163.com>
2020-04-03 11:58:16 +0800

    随机字符串

--- themes/next/source/js/src/DigitalRain.js
+++ themes/next/source/js/src/DigitalRain.js
@@ -1,58 +1,53 @@
-window.onload = function(){
-  //获取画布对象
+window.onload = function() {
   var canvas = document.getElementById("canvas");
-  //获取画布的上下文
-  var context =canvas.getContext("2d");
+  var context = canvas.getContext("2d");
   var s = window.screen;
-  var W = canvas.width = s.width;
+  var W = (canvas.width = s.width);
   var H = canvas.height;
-  //设置canvas的宽度和高度
   canvas.width = W;
   canvas.height = H;
-  //每个文字的字体大小
   var fontSize = 20;
-  //计算列
-  var colunms = Math.floor(W /fontSize);
-  //记录每列文字的y轴坐标
+  var columns = Math.floor(W / fontSize);
   var drops = [];
-  //给每一个文字初始化一个起始点的位置
-  for(var i=0;i<colunms;i++){
-      drops.push(0);
+  for (var i = 0; i < columns; i++) {
+    drops.push(0);
   }
-  //运动的文字
-  var str ="金卡灰色空间的哈就开始";
-  var strList = [
-    '哈时间快点哈就开始打哈健康',
-    '从保存西柏坡',
-    '斗篷山覅帕斯颇高我破讽德诵功',
-  ]
-  //绘画的函数
-  function draw(){
-      context.fillStyle = "rgba(238,238,238,.08)";//遮盖层
-      context.fillRect(0,0,W,H);
-      //给字体设置样式
-      context.font = "600 "+fontSize+"px  Georgia";
-      //给字体添加颜色
-      context.fillStyle = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"][parseInt(Math.random() * 10)];//randColor();可以rgb,hsl, 标准色，十六进制颜色
-      //写入画布中
-      for(var i=0;i<colunms;i++){
-          var index = Math.floor(Math.random() * str.length);
-          var x = i*fontSize;
-          var y = drops[i] *fontSize;
-          context.fillText(str[index],x,y);
-          //如果要改变时间，肯定就是改变每次他的起点
-          if(y >= canvas.height && Math.random() > 0.99){
-              drops[i] = 0;
-          }
-          drops[i]++;
+  function getRandomStr(minLength, maxLength) {
+    var result = [];
+    for (var i = minLength; i < maxLength; i++) {
+      var ranNum = Math.ceil(Math.random() * 25);
+      result.push(String.fromCharCode(65 + ranNum));
+    }
+    return result.join("");
+  }
+
+  var str = getRandomStr(10, 20);
+  function draw() {
+    context.fillStyle = "rgba(238,238,238,.08)";
+    context.fillRect(0, 0, W, H);
+    context.font = "600 " + fontSize + "px  Georgia";
+    context.fillStyle = randColor();
+    for (var i = 0; i < columns; i++) {
+      var index = Math.floor(Math.random() * str.length);
+      var x = i * fontSize;
+      var y = drops[i] * fontSize;
+      context.fillText(str[index], x, y);
+      if (y >= canvas.height && Math.random() > 0.99) {
+        drops[i] = 0;
       }
-  };
-  function randColor(){//随机颜色
-      var r = Math.floor(Math.random() * 256);
-      var g = Math.floor(Math.random() * 256);
-      var b = Math.floor(Math.random() * 256);
-      return "rgb("+r+","+g+","+b+")";
+      drops[i]++;
+    }
   }
-  draw();
-  setInterval(draw,35);
+  function randColor() {
+    return (
+      "rgb(" +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      "," +
+      Math.floor(Math.random() * 256) +
+      ")"
+    );
+  }
+  setInterval(draw, 55);
 };
